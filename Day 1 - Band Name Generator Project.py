20d35a0bc22b66aa4033f5d2619a3a132a16a772
RagingMACHO <amigueldelara@gmail.com>
2024-10-08 20:37:32 +1300

    Delete Day 1 - Band Name Generator Project.py

--- Day 1 - Band Name Generator Project.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band name Generator")
-city = input("Which city did you grow up in?\n")
-pet = input("What's your pet's name\n")
-print("Your band name could be " + city + " " + pet)
